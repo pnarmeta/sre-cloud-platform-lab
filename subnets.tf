922d354b38a04d0ae5d070cee13759dd0416de50
pnarmeta <prasanna.narmeta@gmail.comm>
2026-09-18 15:07:26 -0400

    Add multi-AZ public subnets

--- subnets.tf
+++ subnets.tf
@@ -10,3 +10,16 @@ resource "aws_subnet" "public_1" {
     ManagedBy   = "Terraform"
   }
 }
+
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.sre_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "sre-public-subnet-2"
+    Environment = "learning"
+    ManagedBy   = "Terraform"
+  }
+}
